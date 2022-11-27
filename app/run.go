bb9a2130a7eb9169ff7d1b2f51570f09f8441c66
iskra <iskrapw@gmail.com>
2022-11-27 22:34:16 +0100

    error wrapping

--- app/run.go
+++ app/run.go
@@ -1,24 +1,32 @@
 package app
 
-import "log"
+import (
+	"log"
+
+	"github.com/so5dz/utils/misc"
+)
+
+const _SoundConnectionError = "unable to connect to sound server"
+const _DataServerStartError = "unable to start data server"
+const _ExtraServerStartError = "unable to start extra server"
 
 func (app *ModemApplication) Run() error {
 	log.Println("Connecting to sound server")
 	err := app.soundClient.Connect()
 	if err != nil {
-		return err
+		return misc.WrapError(_SoundConnectionError, err)
 	}
 
 	log.Println("Starting data server")
 	err = app.dataServer.Start()
 	if err != nil {
-		return err
+		return misc.WrapError(_DataServerStartError, err)
 	}
 
 	log.Println("Starting extra server")
 	err = app.extraServer.Start()
 	if err != nil {
-		return err
+		return misc.WrapError(_ExtraServerStartError, err)
 	}
 
 	log.Println("MARDES-modem started")
--- main.go
+++ main.go
@@ -7,6 +7,10 @@ import (
 	"github.com/so5dz/utils/misc"
 )
 
+const _ConfigLoadError = "unable to read/parse configuration"
+const _AppInitializationError = "unable to initialize application"
+const _AppStartError = "unable to start application"
+
 func main() {
 	misc.WrapMain(mainWithError)()
 }
@@ -14,19 +18,19 @@ func main() {
 func mainWithError() error {
 	cfg, err := config.LoadConfigFromArgs[modemconfig.Config]()
 	if err != nil {
-		return err
+		return misc.WrapError(_ConfigLoadError, err)
 	}
 
 	var app app.ModemApplication
 
 	err = app.Initialize(cfg)
 	if err != nil {
-		return err
+		return misc.WrapError(_AppInitializationError, err)
 	}
 
 	err = app.Run()
 	if err != nil {
-		return err
+		return misc.WrapError(_AppStartError, err)
 	}
 
 	misc.BlockUntilInterrupted()
